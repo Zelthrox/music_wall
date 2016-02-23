969fb0269d92a1bd0a8f0ed38d81d771edd1e3b3
Andy Kwan <zelthrox@gmail.com>
2016-02-23 09:36:11 +0000

    version1

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,25 @@
 get '/' do
   erb :index
 end
+
+get '/musics' do
+  @musics = Music.all
+  erb :"musics/index"
+end
+
+get '/musics/new' do
+  erb :"musics/new"
+end
+
+post '/musics' do
+  @musics = Music.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @musics.save
+    redirect '/musics'
+  else
+    erb :"musics/new"
+  end
+end
---
+++ app/models/music.rb
@@ -0,0 +1,6 @@
+class Music < ActiveRecord::Base
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :url, presence: true
+
+end
